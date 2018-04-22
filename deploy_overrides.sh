1d3f96c676ac4283d61f183ec2526b2975b1a8ae
Michael Storm <mstorm7@gmail.com>
2018-04-21 20:20:51 -0700

    Move overrides to their own directory

--- deploy_overrides.sh
+++ deploy_overrides.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
-cp _analytics.jade openbudgetoakland/_src
-cp _data.json openbudgetoakland/_src
-cp _footer.jade openbudgetoakland/_src
-cp _harp.json openbudgetoakland/_src
-cp config.js openbudgetoakland/_src/js
-cp codeforsf.png openbudgetoakland/_src/images/global
+cp overrides/_analytics.jade openbudgetoakland/_src
+cp overrides/_data.json openbudgetoakland/_src
+cp overrides/_footer.jade openbudgetoakland/_src
+cp overrides/_harp.json openbudgetoakland/_src
+cp overrides/config.js openbudgetoakland/_src/js
+cp overrides/codeforsf.png openbudgetoakland/_src/images/global
