4991e524a6548151d60df71719c9f44d2e353936
Michael Storm <mstorm7@gmail.com>
2018-04-21 19:57:10 -0700

    Override analytics code

--- deploy_overrides.sh
+++ deploy_overrides.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -xe
+cp _analytics.jade openbudgetoakland/_src
 cp _data.json openbudgetoakland/_src
-cp _harp.json openbudgetoakland/_src
 cp _footer.jade openbudgetoakland/_src
+cp _harp.json openbudgetoakland/_src
 cp config.js openbudgetoakland/_src/js
 cp codeforsf.png openbudgetoakland/_src/images/global
