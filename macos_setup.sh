db55653df76aa58f32430f01d37ef81767a2e0d8
Michael Storm <mstorm7@gmail.com>
2018-04-29 10:43:57 -0700

    Install python in macos_setup.sh

--- macos_setup.sh
+++ macos_setup.sh
@@ -1,21 +1,14 @@
 #!/bin/bash -xe
-# Set up
 git submodule update --init
 cd openbudgetoakland/_src
 
-# Install node
 brew install node
-
-# Install fswatch
 brew install fswatch
+brew install python
 
-# Install yarn
 npm install yarn
 
-# Install harp
 yarn global add harp
-
-# Install node dependencies
 yarn install
 
 cd ../..
